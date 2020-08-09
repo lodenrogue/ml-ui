85bdcebd02cd4040b9301fb48024e1ffecf14cd8
Miguel Hernandez <phyzix5761@gmail.com>
2020-08-09 15:31:02 -0400

    Added setting request csv path

--- main/csv/csv_view.py
+++ main/csv/csv_view.py
@@ -1,25 +1,31 @@
 import PyQt5.QtWidgets as qtw
-from main.csv.file import FileService
+from main.csv.csv_service import CsvService
 
 
 class CsvView():
 
 	def __init__(self):
-		self.file_service = FileService()
+		self.csv_service = CsvService()
 
 
 	def create_view(self):
 		label = qtw.QLabel('Path to csv:')
 		self.csv_name_box = qtw.QLineEdit()
+		self.csv_name_box.textChanged.connect(self.text_changed)
 
 		button = qtw.QPushButton('Find')
-		button.clicked.connect(lambda: self.file_service.get_file(self.get_file_callback))
+		button.clicked.connect(lambda: self.csv_service.get_file(self.get_file_callback))
 
 		layout = qtw.QHBoxLayout()
 		layout.addWidget(label)
 		layout.addWidget(self.csv_name_box)
 		layout.addWidget(button)
 		return layout
+
+	
+	def text_changed(self):
+		new_text = self.csv_name_box.text()
+		self.csv_service.text_changed(new_text)
 
 
 	def get_file_callback(self, fname):
--- main/csv/file.py
+++
@@ -1,7 +0,0 @@
-import PyQt5.QtWidgets as qtw
-
-class FileService:
-
-	def get_file(self, callback):
-		fname = qtw.QFileDialog.getOpenFileName(None, '*')
-		callback(fname[0])
